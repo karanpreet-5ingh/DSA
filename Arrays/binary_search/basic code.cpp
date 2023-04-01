102969833008e767e76491fc9a7a5b49ff05ced4
karanpreet-singh-iic <kpreet99900@gmail.com>
2023-04-02 00:56:35 +0530

    re aranging stuff

---
+++ Arrays/binary_search/basic code.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int binary_search(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+
+        if (key == arr[mid])
+        {
+            return mid;
+        }
+
+        if (key < arr[mid])
+        {
+            end = mid - 1;
+        }
+
+        else
+        {
+            start = mid + 1;
+        }
+
+        mid = (start + end) / 2;
+    }
+
+    return -1; //if key is not present in the aray
+}
+
+int main()
+{
+    int even[4] = {2,3,7,5};
+    int odd[5] = {2,5,3,6,7};
+    int index  = binary_search(even, 4, 2);
+    cout<< index<<endl;
+
+}
